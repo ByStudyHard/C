f49ebef3ccba20b382951239229860fedcf26c2e
ByStudyHard <bystudyhard@qq.com>
2018-02-10 23:33:43 +0800

    2018-02-10 at home

--- mazhao/test1-2.c
+++ mazhao/test1-2.c
@@ -2,17 +2,13 @@
 /* 输入包裹的重量，计算并显示输出相应的费用。
  * 学生：学Bug
  * 微信公众号：ByStudyHard	*/
-
 //互评作业,printf单一出口修改
 int main()
 {
 	float cost, weight,discount;
-
 	printf("请输入重量(Kg)：");
 	scanf("%f",&weight) ;
-
-	switch(((int)weight)/5){
-		
+	switch((int)weight/5){
 		case 0:
 			discount = 5.0;
 			cost = 0.95 * weight * 5;
