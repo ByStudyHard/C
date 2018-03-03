d0464f9750f021a02948a51719284ced76df4c1d
ByStudyHard <bystudyhard@qq.com>
2018-03-03 22:38:39 +0800

    2018-3-3 At home

--- C++/p3-17.cpp
+++ C++/p3-17.cpp
@@ -2,9 +2,21 @@
  * 学生：学Bug
  * 微信公众号：ByStudyHard	*/
 #include <iostream>
+#include <complex>
+#include <string>
 using namespace std;
+template <class T>
+void printer(complex <T>a)
+{
+	string str1("real is "),str2 = "image is ";
+	cout << str1 << a.real() << ',' << str2 << a.imag() << endl;
+}
 int main()
 {
+	int i(0);
+	complex <int> num1(2,3);
+	complex <double> num2(3.5,4.5);
+	printer(num1);
+	printer(num2);
 	return 0;
 }
-
--- C++/p3-18.cpp
+++ C++/p3-18.cpp
@@ -3,8 +3,24 @@
  * 微信公众号：ByStudyHard	*/
 #include <iostream>
 using namespace std;
+template <typename T>				//使用typename替代class
+T max1(T m1,T m2)				//求最大值
+{
+	return(m1 > m2)?m1:m2;
+}
+template <typename T>				//必须重写
+T min1(T m1,T m2)				//求最小值
+{
+	return(m1 < m2)?m1:m2;
+}
 int main()
 {
+	cout << max1("ABC","ABD") << "," << min1("ABC","ABD") << "," 
+	     << min1('W','T') << "," << min1(2.0,5.);
+	cout << "\t" << min1<double>(8.5,6) << "," << min1(8.5,(double)6) 
+	     << "," << max1((int)8.5,6);
+	cout << "\t" << min1<int>(2.3,5.8) << "," << max1<int>('a','y') 
+	     << "," << max1<char>(95,121) << endl;
 	return 0;
 }
 
